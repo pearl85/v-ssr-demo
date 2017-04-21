1c0e4476baee89494e8ae56cad12242e1bd447da
pearl85 <pearlsankar85@gmail.com>
2017-04-20 23:32:36 -0500

    updated

--- src/component/Film.vue
+++ src/component/Film.vue
@@ -2,7 +2,7 @@
   <section id="section_parent">
    <section class="col-lg-12
       col-md-12 col-sm-12 col-xs-12 pt10 p0" >
-      <article class="col-lg-3 col-md-4 col-sm-4 col-xs-12 wag-product-card-width_b" v-for="film in films">
+      <article class="col-lg-3 col-md-4 col-sm-4 col-xs-12 film-width_b" v-for="film in films">
          
       </article>
       <hr class="col-xs-12 col-md-12 col-sm-12 col-lg-12 p0 mt0">
